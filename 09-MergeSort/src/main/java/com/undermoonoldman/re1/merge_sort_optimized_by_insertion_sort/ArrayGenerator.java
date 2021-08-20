aa7baa3f7b4481fd4e8bcf2e0a362e0e1bc96770
Arthur <undermoonoldman@gmail.com>
2021-08-20 22:59:33 +0800

    完成第九章代码

---
+++ 09-MergeSort/src/main/java/com/undermoonoldman/re1/merge_sort_optimized_by_insertion_sort/ArrayGenerator.java
@@ -0,0 +1,36 @@
+package com.undermoonoldman.re1.merge_sort_optimized_by_insertion_sort;
+
+import java.util.Random;
+
+/**
+ * @author arthurmeng
+ */
+public class ArrayGenerator {
+
+    private ArrayGenerator(){}
+
+    public static Integer[] generateOrderedArray(int n){
+
+        Integer[] arr = new Integer[n];
+        for(int i = 0; i < n; i ++) {
+            arr[i] = i;
+        }
+        return arr;
+    }
+
+    /***
+     * 生成一个长度为 n 的随机数组，每个数字的范围是 [0, bound)
+     * @param n
+     * @param bound
+     * @return
+     */
+    public static Integer[] generateRandomArray(int n, int bound){
+
+        Integer[] arr = new Integer[n];
+        Random rnd = new Random();
+        for(int i = 0; i < n; i ++) {
+            arr[i] = rnd.nextInt(bound);
+        }
+        return arr;
+    }
+}
